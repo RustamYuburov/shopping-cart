5e22d40624fa03adefb02c0d6431163afcb26542
Rustam Yuburov <rt138328@gmail.com>
2021-08-29 18:02:02 +0600

    Finished project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Footer from './components/Footer';
+import { BrowserRouter } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
+import Footer from './components/Footer/Footer';
+import Routes from './Routes';
 
 function App() {
+  const [shoppingCart, setShoppingCart] = useState([]);
+
+  const addItemToCart = (book) => {
+    const itemIndex = shoppingCart.findIndex(
+      (item) => +item.ISBN === +book.ISBN
+    );
+    if (itemIndex > -1) {
+      const newCart = shoppingCart.slice();
+      newCart[itemIndex].quantity++;
+      setShoppingCart(newCart);
+    } else {
+      setShoppingCart([...shoppingCart, book]);
+    }
+  };
+
+  const removeItemFromCart = (book, removeAll) => {
+    const itemIndex = shoppingCart.findIndex(
+      (item) => +item.ISBN === +book.ISBN
+    );
+    if (itemIndex > -1) {
+      const newCart = shoppingCart.slice();
+      removeAll ? newCart.splice(itemIndex, 1) : newCart[itemIndex].quantity--;
+      setShoppingCart(newCart);
+    } else {
+      console.error(
+        "Couldn't remove item from shopping cart: There's no such item in the shopping cart."
+      );
+    }
+  };
+
   return (
-    <div className="App">
-      <h1>Shopping Cart App</h1>
+    <BrowserRouter>
+      <NavBar shoppingCart={shoppingCart.length} />
+      <Routes
+        shoppingCart={shoppingCart}
+        addItemToCart={addItemToCart}
+        removeItemFromCart={removeItemFromCart}
+      />
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
